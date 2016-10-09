551a86cc5efe0e10675bab0b8598741f33e2399b
Ronaldo Gomes Silva <ronaldogomes13@gmail.com>
2016-10-08 21:47:57 -0300

    Population established

--- src/genetic/Populacao.java
+++ src/genetic/Populacao.java
@@ -1,19 +1,29 @@
 package genetic;
 
+import java.util.ArrayList;
 
+import emc.Curso;
+import emc.Disciplina;
+import emc.Estudante;
+import emc.Professor;
+import emc.Sala;
+import emc.TimeSlots;
+import emc.TipoSala;
 
 public class Populacao {
-	
+
 	private int sizePopulation;
 	Cromossomo cromossomo[];
-	
-	Populacao(int sizePopulation){
-		
+
+	Populacao(int sizePopulation, ArrayList<Curso> cursosEMC, ArrayList<Disciplina> DisciplinasEMC,
+				ArrayList<Estudante> alunosEMC, ArrayList<Professor> professoresEMC, ArrayList<Sala> salasEMC,
+				ArrayList<TimeSlots> listaTimeSlots, ArrayList<TipoSala> tipoSalaEMC) {
 		this.setSizePopulation(sizePopulation);
-		for (int i =0;i<sizePopulation;i++){
-			//cromossomo[i] = new Cromossomo();	
+		for (int i = 0; i < sizePopulation; i++) {
+			cromossomo[i] = new Cromossomo(cursosEMC, DisciplinasEMC, alunosEMC, professoresEMC, salasEMC,
+					listaTimeSlots, tipoSalaEMC);
 		}
-		
+
 	}
 
 	public int getSizePopulation() {
@@ -25,6 +35,3 @@ public class Populacao {
 	}
 
 }
-	
-
-
