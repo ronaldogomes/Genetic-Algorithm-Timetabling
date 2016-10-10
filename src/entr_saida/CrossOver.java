5ddded1e9e471f5ea5d9622ce02c178fe1184d78
gigochaib <gigochaibs@gmail.com>
2016-10-10 01:17:47 -0300

    Crossover inicial

--- src/entr_saida/CrossOver.java
+++ src/entr_saida/CrossOver.java
@@ -2,6 +2,28 @@ package entr_saida;
 import emc.*;
 import genetic.*;
 
-public class CrossOver {
 
+public class CrossOver {
+	
+		public Gene child1, child2;
+		CrossOver(Gene parent1, Gene parent2){
+
+				child1 = new Gene(parent2.getProfessor(), parent2.getSala(),
+							parent2.getDisciplina(), parent1.getAlunos(),
+							parent1.getTimeslots(), parent1.getCurso());
+				 child2 = new Gene(parent1.getProfessor(), parent1.getSala(),
+							parent1.getDisciplina(), parent2.getAlunos(),
+							parent2.getTimeslots(), parent2.getCurso());
+			}
+	
+		
+		
+		public Gene retornaFilho1(){
+			return child1;
+				}
+		public Gene retornaFilho2(){
+			return child2;
+				}
 }
+
+
